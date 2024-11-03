38b1678c8ec290ba9284e0f7c4d34cbb8e6d22a9
Srikar Bachali <srikarbachali@gmail.com>
2024-11-03 14:02:11 +0530

    Update MainActivity.java

--- FarmlandSecurity/app/src/main/java/com/finalyearproject/farmlandsecurity/MainActivity.java
+++ FarmlandSecurity/app/src/main/java/com/finalyearproject/farmlandsecurity/MainActivity.java
@@ -1,33 +1,17 @@
 package com.finalyearproject.farmlandsecurity;
 
 import android.annotation.SuppressLint;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hivemq.client.mqtt.MqttClient;
-import com.hivemq.client.mqtt.MqttClientState;
-import com.hivemq.client.mqtt.MqttGlobalPublishFilter;
-import com.hivemq.client.mqtt.datatypes.MqttQos;
 import com.hivemq.client.mqtt.mqtt3.Mqtt3AsyncClient;
-import com.hivemq.client.mqtt.mqtt3.Mqtt3Client;
-import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAck;
-import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
-import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
-import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 
 public class MainActivity extends AppCompatActivity {
